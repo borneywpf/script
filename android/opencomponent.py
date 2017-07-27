391361f328b3474a4f6a4bf3412aa9811b77c4c8
borney <borneywpf@gmail.com>
2017-07-27 19:25:50 +0800

    编辑脚步

--- android/opencomponent.py
+++ android/opencomponent.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
-# coding= utf-8
+
+# -*- coding:utf-8 -*-
+
+# Author:borney
+# Date:2017-06-6
+
 import os
 import sys
-import commands
-from pprint import pprint  
+import re
 import xml.etree.ElementTree as ET
 
-
-# 脚本用途：用于检查在manifest中注册了没有实现的android组件
-# 使用方法：第一个参数指定manifest文件，第二个参数指定code目录
-
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[31m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def attrib_value(attrib_dict, attrib):
     for (k,v) in attrib_dict.items():
@@ -100,77 +110,71 @@ def componts(tree, package_name):
                 r_componts.append(component)
 
     return r_componts
+    
+def print_compont(com):
+    print "=" * 99
+    print "%-14s: %s" % ("Type", com["tag"])
+    print "%-14s: %s" % ("Name", com["name"])
+        
+    if com["exported"] != None:
+        print "%-14s: %s" % ("Exported", com["exported"])
+        
+    if com["illegal"] == True:
+        print (bcolors.ERROR + "%-14s: %s" + bcolors.ENDC) % ("Illegal", com["illegal"])
+    else:
+        print (bcolors.OKGREEN + "%-14s: %s" + bcolors.ENDC) % ("Illegal", com["illegal"])
+        
+    if com["permission"] != None:
+        print "%-14s: %s" % ("Permission", com["permission"])
+        
+    if com["target_activity"] != None:
+        print "%-14s: %s" % ("targetActivity", com["target_activity"])
+        
+    filters = com["filter"]
+    if len(filters):
+        print "%-14s:" % ("Filters")
+        for f in filters:
+            print "%-14s: %s" % ("", "-" * 83)
+            if len(f["action"]):
+                print "%-14s: %-14s: %s" % ("", "action", f["action"])
+            if len(f["category"]):
+                print "%-14s: %-14s: %s" % ("", "category", f["category"])
+            if len(f["data"]):
+                print "%-14s: %-14s: %s" % ("", "data", f["data"])
 
-def check_component(code, component):
-    name = component.split('.')[-1]
-    return commands.getstatusoutput("find " + code + " -name *" + name + ".*")
-
-def unuse_activity_alias(self_components, code, package_name, target_activity):
-    is_exist_target = False
-    for com in self_components:
-        if in_package(package_name, com["name"]):
-            if com["tag"] != "activity-alias":
-                if com["name"] == target_activity:
-                    status, result = check_component(code, target_activity)                    
-                    if result and status == 0:
-                        is_exist_target = True
-                else: 
-                    continue
-    return is_exist_target
-
-def unuse_components(self_components, code, package_name):
-    r_un_use_component = []
-    for com in self_components:
-        if in_package(package_name, com["name"]):
-            if com["tag"] == "activity-alias":
-                if com["target_activity"] != None:
-                    if not unuse_activity_alias(self_components, code, package_name, com["target_activity"]):
-                        r_un_use_component.append(com)
-                else:
-                    r_un_use_component.append(com)
-            else:
-                status, result = check_component(code, com["name"])
-
-                if not result and status == 0:
-                    r_un_use_component.append(com)
-    return r_un_use_component
+def print_componts(componts, tags):
+    for com in componts:
+        if len(tags) and com["tag"] not in tags:
+            continue
+        print_compont(com)
+        
 
 def verify_args():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         sys.exit(1)
 
     manifest_file = os.path.abspath(sys.argv[1])
-    code_dir = os.path.abspath(sys.argv[2])
+    
+    tags = []
+    if len(sys.argv) is 3:
+        tags.extend(sys.argv[2].split(','))
 
     if os.path.exists(manifest_file) and os.path.isfile(manifest_file) and manifest_file.endswith(
             'AndroidManifest.xml'):
-        print manifest_file
+        print "%-14s> %s" % ("Manifest", manifest_file)
     else:
         sys.exit(1)
+    
+    print "%-14s> %s" % ("Types", tags)
 
-    if os.path.exists(code_dir) and os.path.isdir(code_dir):
-        print code_dir
-    else:
-        sys.exit(1)
-    return manifest_file, code_dir
-
+    return manifest_file, tags
 
 def main():
-    manifest, code = verify_args()
+    manifest, tags = verify_args()
     
     tree = ET.ElementTree(file=manifest)
     
-    package = package_name(tree)
-    
-    unuse_component = unuse_components(componts(tree, package), code, package)
-    
-    if len(unuse_component):
-        print "查找到非法注册的组件:"
-        pprint(unuse_component)
-        sys.exit(1)
-    else:
-        print "恭喜你，没有找到非法注册的组件!!!"
-        sys.exit(0)
+    print_componts(componts(tree, package_name(tree)), tags)
 
 if __name__ == '__main__':
     main()
